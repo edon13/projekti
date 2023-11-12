327ad62ceab970c6218880380eead8e800903796
edon13 <75705441+edon13@users.noreply.github.com>
2023-11-12 22:02:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 console.log("hello world");
-alert("hello");
+
+const button1 = document.getElementById('button1');
+const divbox = document.getElementsByClassName('flexcontainer')
+
+button1.addEventListener('click',function Detyra() {
+    //alert("tu qi nana")
+    
+    //button1.style.backgroundColor = '#112D4E';
+    
+});
+function right(){
+    divbox.style.marginLeft = 30+"px";
 
 
-function Detyra(){
-    alert("tu qi nana");
 }
+right();
+
+
+
 
 
