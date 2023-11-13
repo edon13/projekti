0db10bce453e743693d44560166e5ed892be23d0
edon13 <75705441+edon13@users.noreply.github.com>
2023-11-13 01:17:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 console.log("hello world");
 
+
+
 const button1 = document.getElementById('button1');
 const divbox = document.getElementsByClassName('flexcontainer')
 
 button1.addEventListener('click',function Detyra() {
     //alert("tu qi nana")
-    
+    console.log("tu qi nana");
     //button1.style.backgroundColor = '#112D4E';
     
 });
-function right(){
-    divbox.style.marginLeft = 30+"px";
+function spawnContainer(){
+    const flexContainer = documentById('flexcontainer');
+    const newContainer = flexContainer.children[0].cloneNode(true);
+
+    flexContainer.appendChild(newContainer);
+
+    setInterval(spawnContainer,3000)
 
 
 }
-right();
-
-
-
 
 
