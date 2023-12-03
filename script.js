f8e2db20180f76e39bdf7dbe7540465e427305ca
Edon <edonemini449@gmail.com>
2023-12-03 14:25:12 +0100

    kalendari

--- script.js
+++ script.js
@@ -3,23 +3,34 @@ console.log("hello world");
 
 
 const button1 = document.getElementById('button1');
-const divbox = document.getElementsByClassName('flexcontainer')
 
-button1.addEventListener('click',function Detyra() {
+
+
+
+button1.addEventListener('click',function Detyra()  {
     //alert("tu qi nana")
     console.log("tu qi nana");
     //button1.style.backgroundColor = '#112D4E';
     
+
+        var x = document.getElementById("rezervimi");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      
+    
 });
-function spawnContainer(){
-    const flexContainer = documentById('flexcontainer');
-    const newContainer = flexContainer.children[0].cloneNode(true);
-
-    flexContainer.appendChild(newContainer);
-
-    setInterval(spawnContainer,3000)
 
 
-}
+
+
+
+
+
+
+
+
 
 
