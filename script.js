cc7e29da62bbcf0572a501b49b9de9fc5123625d
Anes Asllani <aa67282@ubt-uni.net>
2023-12-10 22:27:58 +0100

    ndryshimi i sliderit ne index ku kam kopjuar ticketat prej bookT.html me js dhe ndryshimi i pak css

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("hello");
+
+
+
+
+function fetchBookTContent() {
+    fetch('bookT.html')
+        .then(response => response.text())
+        .then(html => {
+            const tempElement = document.createElement('div');
+            tempElement.innerHTML = html;
+
+            const ticketDivs = tempElement.querySelectorAll('.ticket');
+
+            const sliderContainer = document.getElementById('flexcontainer');
+            const sliderContainer2 = document.getElementById('flexcontainer2');
+
+            ticketDivs.forEach(ticketDiv => {
+                // Append to both containers
+                sliderContainer.appendChild(ticketDiv.cloneNode(true));
+                sliderContainer2.appendChild(ticketDiv.cloneNode(true));
+            });
+        })
+        .catch(error => console.error('Error fetching bookT.html:', error));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchBookTContent();
+});
