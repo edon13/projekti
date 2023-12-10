e9b1d2db274f6049a93e241c30b0968f3429695a
Anes Asllani <aa67282@ubt-uni.net>
2023-12-10 19:54:11 +0100

    shtimi i folder style dhe ndryshimi i nav bar

--- loginpage/login.js
+++
@@ -1,7 +0,0 @@
-function toggleForm() {
-    var loginform = document.getElementById('loginform');
-    var signupform = document.getElementById('signupform');
-
-    loginform.style.display = (loginform.style.display === 'none') ? 'block' : 'none';
-    signupform.style.display = (signupform.style.display === 'none') ? 'block' : 'none';
-}
