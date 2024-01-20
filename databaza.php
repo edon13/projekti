5bf2cee1a94cf17e316af5b96f18cd7404561c9a
Anes Asllani <aa67282@ubt-uni.net>
2024-01-20 15:51:35 +0100

    Editimi i konektimit me databaz

--- databaza.php
+++ databaza.php
@@ -1,16 +1,19 @@
 <?php
+    $hostname = '127.0.0.1';
+    $username = 'root';
+    $password = '';
+    $database = 'booknow';
+
+    try {
+        $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ));
+        echo "Connection succsess!";
         
-        $hostname = '127.0.0.1';
-        $username = 'root';
-        $password = [null];
-        $database = 'booknow';
 
-     
-        $conn = new mysqli($hostname, $username, $password, $database);
+        
 
-       
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $conn->close();
-        ?>
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
